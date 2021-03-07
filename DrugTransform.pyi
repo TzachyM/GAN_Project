9f37924e25083a7eb5a69a035fd3207594d8fa33
Tzachy <tzachy11@gmail.com>
2021-03-07 15:17:28 +0200

    Initial commit

---
+++ DrugTransform.pyi
@@ -0,0 +1,11 @@
+import torch
+from torch.datasets import datasets
+from torch.utils.data import DataLoader
+import os
+import numpy as np
+
+
+
+
+if __name__ == "__main__":
+    torch.manual_seed(17)
